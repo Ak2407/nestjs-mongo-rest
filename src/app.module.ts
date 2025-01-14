6c4c2b0d07160a63dccd99c19a041ca075de8f21
Akshit Gupta <akshu.24gupta@gmail.com>
2025-01-14 12:06:30 +0530

    user operations completed and tested

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OrgModule } from './org/org.module';
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URL),
     OrgModule,
+    UserModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/users/user.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './user.service';
+import { User } from './user.schema';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // Creating a new User
+  @Post()
+  async create(@Body() user: User): Promise<User> {
+    return this.usersService.create(user);
+  }
+
+  // Finding all Users
+  @Get()
+  async findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  // Finding by ID
+  @Get(':id')
+  async findById(@Param('id') id: string): Promise<User> {
+    return this.usersService.findById(id);
+  }
+
+  // Updating
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() user: User): Promise<User> {
+    return this.usersService.update(id, user);
+  }
+
+  // Deleting
+  @Delete(':id')
+  async delete(@Param('id') id: string): Promise<User> {
+    return this.usersService.delete(id);
+  }
+}
